cf133efa50816704cd2b496039993de186d1b28a
danil-tr <daniltrvit@gmail.com>
2024-01-22 17:11:35 +0300

    Add fixes after pre-commit install

--- iris_log_reg/infer.py
+++ iris_log_reg/infer.py
@@ -1,16 +1,20 @@
 import os
+
 import joblib
 import pandas as pd
 from sklearn import metrics
-from sklearn.metrics import classification_report
+
 
 def infer_model(model, test_df: pd.DataFrame) -> pd.DataFrame:
-    test_df.drop(columns=test_df.columns[0], axis=1, inplace=True)  
+    test_df.drop(columns=test_df.columns[0], axis=1, inplace=True)
     X = test_df.iloc[:, 0:4]
     y = test_df["Species"]
 
     prediction = model.predict(X)
-    print('The accuracy of the Logistic Regression is', metrics.accuracy_score(prediction,y))
+    print(
+        "The accuracy of the Logistic Regression is",
+        metrics.accuracy_score(prediction, y),
+    )
     return prediction
 
 
@@ -25,5 +29,7 @@ if __name__ == "__main__":
     model = joblib.load(model_path)
 
     prediction = pd.DataFrame(infer_model(model, test_df))
-    
-    pd.DataFrame(prediction).to_csv(prediction_path, sep=",", header=False, encoding="utf-8")
+
+    pd.DataFrame(prediction).to_csv(
+        prediction_path, sep=",", header=False, encoding="utf-8"
+    )
--- iris_log_reg/train.py
+++ iris_log_reg/train.py
@@ -1,19 +1,20 @@
-import numpy as np 
-import pandas as pd
 import os
+
 import joblib
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 
 def train_model(train_df: pd.DataFrame) -> LogisticRegression:
-    train_df.drop(columns=train_df.columns[0], axis=1, inplace=True)  
+    train_df.drop(columns=train_df.columns[0], axis=1, inplace=True)
     X = train_df.iloc[:, 0:4]
     y = train_df["Species"]
 
-    model = LogisticRegression(solver='lbfgs', max_iter=200)
+    model = LogisticRegression(solver="lbfgs", max_iter=200)
     model.fit(X, y)
 
     return model
+
 
 if __name__ == "__main__":
     current_file = os.path.realpath(__file__)
